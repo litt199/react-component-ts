63fd7bc7465f76ee44e9bd8f26c1bf6f92a75579
litt199 <1638120490@qq.com>
2020-09-10 17:35:43 +0800

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import React ,{useState}from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MouseTracker from './components/MouseTracker'
-import useMousePosition from './hooks/useMousePosition'
-import useURLLoader from './hooks/useURLLoader'
-import LikeButton from './components/LikeButton'
+import React from 'react'
+import './styles/index.scss'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
-interface IShowResult{
-  message:string;
-  status:string;
-}
-interface IThemeProps{
-  [key:string]:{color:string;background:string}
-}
-const themes:IThemeProps = {
-  "light":{
-    color:"#000",
-    background:"#eee"
-  },
-  "dark":{
-    color:"#fff",
-    background:"#222"
-  }
-}
-export const ThemeContext = React.createContext(themes.light);
-function App() {
-  const [show,setShow] = useState(true);
-  const [data,loading] = useURLLoader("https://dog.ceo/api/breeds/image/random",[show]);
-  const dogResult = data as IShowResult;
-  const positions = useMousePosition()
+const App: React.FC = () => {
   return (
-    
     <div className="App">
-      <ThemeContext.Provider value={themes.dark}>
-        <header className="App-header">
-          <p>
-            <button onClick={()=>{setShow(!show)}}>change</button>
-          </p>
-          {show && <MouseTracker/>}
-          {loading? <p>狗狗加载中...</p>:<img src={dogResult&&dogResult.message}/>}
-          <p>
-              X:{positions.x}   
-              Y:{positions.y}
-          </p>
-          <LikeButton/>
-        </header>
-      </ThemeContext.Provider>
+      <header className="App-header">
+        <Button disabled>hello</Button>
+        <Button className="suto">hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          hello
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+          baidu
+        </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
